1ea1eda6b226056e94619923040504fa2ddc8cdf
Kriti <130234951+tiwarikriti@users.noreply.github.com>
2025-08-12 01:16:36 +0530

    Add files via upload

---
+++ Retail Orders Analysis/Order_details.py
@@ -0,0 +1,55 @@
+import os
+import shutil
+from pathlib import Path
+import zipfile
+import pandas as pd
+import subprocess
+
+# Kaggle API setup
+local_kaggle_json = Path.home() / "Downloads" / "kaggle.json"
+
+kaggle_dir = Path.home() / ".kaggle"
+kaggle_dir.mkdir(exist_ok=True)
+
+shutil.copy(local_kaggle_json, kaggle_dir / "kaggle.json")
+
+
+os.chmod(kaggle_dir / "kaggle.json", 0o600)
+
+subprocess.run([
+    "kaggle", "datasets", "download", "ankitbansal06/retail-orders"
+])
+
+with zipfile.ZipFile("retail-orders.zip", "r") as zip_ref:
+    zip_ref.extractall(".")  # Extracts into current directory
+
+# Load and clean data
+df = pd.read_csv("orders.csv", na_values=['Not Available', 'unknown'])
+
+df.columns = df.columns.str.lower()
+df.columns = df.columns.str.replace(' ', '_')
+
+df['discount'] = (df['list_price']*df['discount_percent'])/100
+df['sale_price'] = df['list_price']-df['discount']
+df['profit'] = df['sale_price'] - df['cost_price']
+
+df['order_date'] = pd.to_datetime(df['order_date'], format="%Y-%m-%d")
+
+# Drop unused columns
+df.drop(columns=['list_price', 'cost_price', 'discount_percent'], inplace=True)
+
+import mysql.connector
+from sqlalchemy import create_engine
+
+# MySQL database connection details
+user = "****"         # your MySQL username
+password = "*****"  # your MySQL password
+host = "localhost"    # or IP address of your MySQL server
+database = "retail_db" # database name
+
+# Create SQLAlchemy engine
+engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
+
+# Export DataFrame to MySQL table
+df.to_sql("df_orders", con=engine, if_exists="append", index=False)
+
